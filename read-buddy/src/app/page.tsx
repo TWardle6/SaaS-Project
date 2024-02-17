ded4e285474accb5b9e907b80e2d02b701baaf74
Thomas Wardle <twardle6@googlemail.com>
2024-02-17 20:44:09 +0000

    Working on landing page

--- read-buddy/src/app/page.tsx
+++ read-buddy/src/app/page.tsx
@@ -1,4 +1,8 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button'
+
 
 export default function Home() {
   return (
@@ -8,6 +12,17 @@ export default function Home() {
           Read Buddy is now public! 🎉
         </p>
       </div>
+      <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+        Chat with your <span className='text-blue-600'>documents</span> in seconds!
+      </h1>
+      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+        Read Buddy is a new way to chat with your documents! Simply upload your PDF file and start asking questions right away.
+      </p>
+      <Link className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5',
+          })}
+          href='/dashboard' target='_blank'>Get started <ArrowRight className='ml-2 h-5 w-5' /></Link>
     </MaxWidthWrapper>
   )
 }
